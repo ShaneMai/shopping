f1188c2ef60c12fc1f3edbf7d1203ee9769b6fd9
Mai Xuân Hòa <84553571+ShaneMai@users.noreply.github.com>
2022-05-06 23:34:27 +0700

    Update header.php

--- templates/frontend/header.php
+++ templates/frontend/header.php
@@ -10,17 +10,7 @@
                 </div>
             </div>
             <div class="col-md-6 ">
-            <div class="col-12 header-right footer-bottom">
-                <ul>
-                    <li><a href="#" class="use1" data-toggle="modal" data-target="#myModal4"><span>Login</span></a>
-
-                    </li>
-                    <li><a class="fb" href="#"></a></li>
-                    <li><a class="twi" href="#"></a></li>
-                    <li><a class="insta" href="#"></a></li>
-                    <li><a class="you" href="#"></a></li>
-                </ul>
-            </div>
+         
         </div>
 
 
