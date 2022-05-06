32d8581908d50eed4fc42a3b25df6950d700e46d
Mai Xuân Hòa <84553571+ShaneMai@users.noreply.github.com>
2022-05-06 23:16:53 +0700

    Update config.php

--- models/config.php
+++ models/config.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_HOST","localhost");
 define("DB_NAME","php0321e2_nhom3");
-define("DB_USER","root");
-define("DB_PWD","");
+define("DB_USER","PHP0321E2_nhom3");
+define("DB_PWD","QGnvmfRyTO");
 ?>
